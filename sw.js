12cdc5cd596ad37eaa9b4cf685665435c38c515b
clearwinggames <80926717+clearwinggames@users.noreply.github.com>
2025-08-22 08:00:28 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,7 @@
 // Change this to your repository name
 var GHPATH = '/ValleyOfMasks';
+
+var IGFPATH = '/ValleyOfMasks/lib/ghtml/igf';
  
 // Choose a different app prefix name
 var APP_PREFIX = 'vom_';
@@ -21,7 +23,57 @@ var URLS = [
   `${GHPATH}/lib/three/models.js`,
   `${GHPATH}/lib/three/support.js`,
   `${GHPATH}/lib/three/three.js`,
-  `${GHPATH}/lib/content/default.png`
+  `${GHPATH}/lib/content/default.png`,
+ 
+  `${IGFPATH}/dirt32.png`,
+  `${IGFPATH}/grass32.png`,
+  `${IGFPATH}/ivanGreatForest.192.dirt32.png`,
+  `${IGFPATH}/ivanGreatForest.192.grass32.png`,
+  `${IGFPATH}/ivanGreatForest.192.ivanGreatForest192_cc_testStairs0.png`,
+  `${IGFPATH}/ivanGreatForest.192.ivanGreatForest192_cc_testStairsDown0.png`,
+  `${IGFPATH}/ivanGreatForest.192.ivanGreatForest192_cc_tile60.png`,
+  `${IGFPATH}/ivanGreatForest.192.stone32.png`,
+  `${IGFPATH}/ivanGreatForest.192.stoneWall32.png`,
+  `${IGFPATH}/ivanGreatForest.192.water1.png`,
+  `${IGFPATH}/ivanGreatForest.192.water10.png`,
+  `${IGFPATH}/ivanGreatForest.192.water11.png`,
+  `${IGFPATH}/ivanGreatForest.192.water12.png`,
+  `${IGFPATH}/ivanGreatForest.192.water14.png`,
+  `${IGFPATH}/ivanGreatForest.192.water15.png`,
+  `${IGFPATH}/ivanGreatForest.192.water13.png`,
+  `${IGFPATH}/ivanGreatForest.192.water2.png`,
+  `${IGFPATH}/ivanGreatForest.192.water3.png`,  
+  `${IGFPATH}/ivanGreatForest.192.water4.png`,
+  `${IGFPATH}/ivanGreatForest.192.water5.png`,
+  `${IGFPATH}/ivanGreatForest.192.water6.png`,
+  `${IGFPATH}/ivanGreatForest.192.water7.png`,
+  `${IGFPATH}/ivanGreatForest.192.water8.png`,
+  `${IGFPATH}/ivanGreatForest.192.water9.png`,
+  `${IGFPATH}/ivanGreatForest.205.htm`,
+  `${IGFPATH}/ivanGreatForest.205.html`,
+  `${IGFPATH}/ivanGreatForest.205.xmlr`,
+  `${IGFPATH}/ivanGreatForest205_cc_testStairs0.png`,
+  `${IGFPATH}/ivanGreatForest205_cc_testStairsDown0.png`,
+  `${IGFPATH}/ivanGreatForest205_cc_tile60.png`,
+  `${IGFPATH}/stone32.png`,
+  `${IGFPATH}/stoneWall32.png`,
+  `${IGFPATH}/water1.png`,
+  `${IGFPATH}/water10.png`,
+  `${IGFPATH}/water11.png`,
+  `${IGFPATH}/water12.png`,
+  `${IGFPATH}/water13.png`,
+ `${IGFPATH}/water14.png`,
+  `${IGFPATH}/water15.png`,
+  `${IGFPATH}/water2.png`,
+  `${IGFPATH}/water3.png`,
+  `${IGFPATH}/water4.png`,
+  `${IGFPATH}/water5.png`,
+  `${IGFPATH}/water6.png`,
+  `${IGFPATH}/water7.png`,
+  `${IGFPATH}/water8.png`,
+  `${IGFPATH}/water9.png`,
+
 ]
+
 
 
