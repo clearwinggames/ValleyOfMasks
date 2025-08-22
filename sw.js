e89243afda4362ff5b20dbb9cba371f5eb4acd26
clearwinggames <80926717+clearwinggames@users.noreply.github.com>
2025-08-22 08:05:36 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@ var APP_PREFIX = 'vom_';
 // you need to change this version (version_01, version_02…). 
 // If you don't change the version, the service worker will give your
 // users the old files!
-var VERSION = 'version_0_0_0_1';
+var VERSION = 'version_0_0_0_2';
  
 // The files to make available for offline use. make sure to add 
 // others to this list
@@ -74,6 +74,7 @@ var URLS = [
   `${IGFPATH}/water9.png`,
 
 ]
+
 
 
 
