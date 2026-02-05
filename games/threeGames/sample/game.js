1c3afdd558748a643edc9e6603bfeb0df80562ca
clearwinggames <80926717+clearwinggames@users.noreply.github.com>
2026-02-05 11:52:15 -0500

    Update game.js

--- games/threeGames/sample/game.js
+++ games/threeGames/sample/game.js
@@ -3,7 +3,8 @@ newThreeScene(
 [ 
 new Grid3D(0, -1, 1, 1, 1, 1, 14, 12, 13).drawSolidCube(0, 0, 0, 14, 1, 12, new Sprite(0, 0, 32, 32, 'brickx.png')),
 new Grid3D(0, -1, 0, 1, 1, 1, 12, 12, 12).drawSolidCube(3, 1, 3, 4, 1, 1, new Sprite(0, 0, 32, 32, 'brickx.png')),
-new Grid3D(0, 1, 0, 1, 1, 1, 12, 12, 12).drawSolidCube(3, 1, 3, 4, 4, 4, new Sprite(0, 0, 32, 32, 'brickx.png')) 
+new Grid3D(0, 0, 0, 1, 1, 1, 1, 1, 1).drawSolidCube(1, 1, 1, 1, 1, 1, new Sprite(0, 0, 32, 32, 'kioskx.png'))
+//new Grid3D(0, 1, 0, 1, 1, 1, 12, 12, 12).drawSolidCube(3, 1, 3, 4, 4, 4, new Sprite(0, 0, 32, 32, 'brickx.png')) 
 
 ]).then(s1 => 
 {
