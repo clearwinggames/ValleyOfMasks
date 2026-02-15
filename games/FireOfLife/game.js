64ed7f8f7602a3bef8f9586496cd9007cdbfd6a3
clearwinggames <80926717+clearwinggames@users.noreply.github.com>
2026-02-15 15:37:41 -0500

    Update game.js

--- games/FireOfLife/game.js
+++ games/FireOfLife/game.js
@@ -1,8 +1,8 @@
 
 
 let first_scene = () => {
-    let firstGrid = new Grid(-128, -128, 32, 32, 24, 24, new Sprite(0, 0, 32, 32, 'default.png'));    
-    let secondGrid =  new Grid(64, 128, 32, 32, 8, 8, new Sprite(0, 0, 32, 32, 'altdefault.png'));
+    let firstGrid = new Grid(-128, -128, 32, 32, 24, 24, new Sprite(0, 0, 32, 32, 'grass32.png'));    
+    let secondGrid =  new Grid(64, 128, 32, 32, 8, 8, new Sprite(0, 0, 32, 32, 'dirt32.png'));
     secondGrid.turnCollisionOn([{ 'x': 128, 'y': 128, 'w':32, 'h':32 }]);
     
     return new Array
@@ -21,4 +21,5 @@ keydownActions.push({ name: 'three', keyval: 'left', action: () => { moveCurrent
 keydownActions.push({ name: 'four', keyval: 'right', action: () => { moveCurrentSceneBy(4, 0); } });
 
 startGameLoop();
+
 
