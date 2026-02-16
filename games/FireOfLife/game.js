442f7a337ec5c5302f1d96d91bfa0615b02911bd
clearwinggames <80926717+clearwinggames@users.noreply.github.com>
2026-02-15 19:39:21 -0500

    Update game.js

--- games/FireOfLife/game.js
+++ games/FireOfLife/game.js
@@ -1,4 +1,7 @@
 
+let canvas = document.getElementById('gameCanvas');
+canvas.style.height = '300px';
+canvas.style.width = '400px';
 
 let first_scene = () => {
     let firstGrid = new Grid(-128, -128, 32, 32, 24, 24, new Sprite(0, 0, 32, 32, 'grass32.png'));    
@@ -26,6 +29,7 @@ setTimeout(() => {
     
     startGameLoop();
 }, 500);
+
 
 
 
