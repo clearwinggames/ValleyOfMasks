fb6fb6a5594c2688c9e42c4698d279158671191a
clearwinggames <80926717+clearwinggames@users.noreply.github.com>
2026-02-17 10:32:23 -0500

    Update game.js

--- games/FireOfLife/game.js
+++ games/FireOfLife/game.js
@@ -12,7 +12,7 @@ let first_scene = () => {
     let firstGrid = new Grid(-128, -128, 32, 32, 24, 24, new Sprite(0, 0, 32, 32, 'grass32.png'));    
     let secondGrid =  new Grid(64, 128, 32, 32, 8, 8, new Sprite(0, 0, 32, 32, 'redstone32.png'));
     firstGrid.turnBoundsOn(canvas);
-    secondGrid.turnCollisionOn([ renderSprite ], (grid, cw, tile) => { alert(cw.x); });
+    secondGrid.turnCollisionOn([ renderSprite ], (grid, cw, tile) => { console.log('works: ' + cw.x.toString()); });
     
     return new Array
     (
@@ -36,6 +36,7 @@ setTimeout(() => {
     
     startGameLoop();
 }, 500);
+
 
 
 
