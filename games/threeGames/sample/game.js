391494547c0dd97ba2d3cd205276e7de63922771
clearwinggames <80926717+clearwinggames@users.noreply.github.com>
2026-02-22 10:09:55 -0500

    Update game.js

--- games/threeGames/sample/game.js
+++ games/threeGames/sample/game.js
@@ -12,11 +12,11 @@ function alternate_first_scene() {
 	let outerwall2 = new Grid3D(coordsStart.x, 0, coordsStart.z, dimensions.x, dimensions.y, dimensions.z, 13, 1, 1)
 	    .drawSolidCube(0, 0, 0, 13, 1, 1, new Sprite(0, 0, 32, 32, 'densetreesx.png'));
 	
-	let outerwall3 = new Grid3D(coordsStart.x, 0, coordsStart.z, dimensions.x, dimensions.y, dimensions.z, 1, 1, 14)
-	    .drawSolidCube(13, 0, 0, 1, 1, 1, new Sprite(0, 0, 32, 32, 'densetreesx.png'));
+	let outerwall3 = new Grid3D(coordsStart.x + 10, 0, coordsStart.z, dimensions.x, dimensions.y, dimensions.z, 1, 1, 14)
+	    .drawSolidCube(0, 0, 0, 1, 1, 14, new Sprite(0, 0, 32, 32, 'densetreesx.png'));
 
-	let outerwall4 = new Grid3D(coordsStart.x, 0, coordsStart.z, dimensions.x, dimensions.y, dimensions.z, 13, 1, 1)
-	    .drawSolidCube(0, 0, 14, 1, 1, 1, new Sprite(0, 0, 32, 32, 'densetreesx.png'));
+	let outerwall4 = new Grid3D(coordsStart.x, 0, coordsStart.z + 10, dimensions.x, dimensions.y, dimensions.z, 13, 1, 1)
+	    .drawSolidCube(0, 0, 0, 13, 1, 1, new Sprite(0, 0, 32, 32, 'densetreesx.png'));
 	
 	let fs = [ 
 				floor,
