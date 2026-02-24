8399fed8088bb77c403a9916e6548f48801b35cb
clearwinggames <80926717+clearwinggames@users.noreply.github.com>
2026-02-24 10:17:02 -0500

    Update game.js

--- games/threeGames/sample/game.js
+++ games/threeGames/sample/game.js
@@ -1,21 +1,22 @@
 let coordsStart = { x: 0, y: 0, z: 0 };
 let dimensions = { x: 1, y: 1, z: 1 };
+let offSet = 1;
 
 function alternate_first_scene() {
 	
-	let floor = new Grid3D(coordsStart.x, -1, 1, dimensions.x, dimensions.y, dimensions.z, 14, 12, 13)
+	let floor = new Grid3D(coordsStart.x - offSet, -1, 0 - offSet, dimensions.x, dimensions.y, dimensions.z, 14, 12, 13)
 	.drawSolidCube(0, 0, 0, 14, 1, 12, new Sprite(0, 0, 32, 32, 'grassxs.png'));
 	
-	let outerwall1 = new Grid3D(coordsStart.x, 0, coordsStart.z, dimensions.x, dimensions.y, dimensions.z, 1, 1, 14)
+	let outerwall1 = new Grid3D(coordsStart.x - offSet, 0, coordsStart.z - offSet, dimensions.x, dimensions.y, dimensions.z, 1, 1, 14)
 	    .drawSolidCube(0, 0, 0, 1, 1, 14, new Sprite(0, 0, 32, 32, 'brickxs.png'));
 
-	let outerwall2 = new Grid3D(coordsStart.x, 0, coordsStart.z, dimensions.x, dimensions.y, dimensions.z, 13, 1, 1)
+	let outerwall2 = new Grid3D(coordsStart.x - offSet, 0, coordsStart.z - offSet, dimensions.x, dimensions.y, dimensions.z, 13, 1, 1)
 	    .drawSolidCube(0, 0, 0, 13, 1, 1, new Sprite(0, 0, 32, 32, 'densetreesxs.png'));
 	
-	let outerwall3 = new Grid3D(coordsStart.x + 10, 0, coordsStart.z, dimensions.x, dimensions.y, dimensions.z, 1, 1, 14)
+	let outerwall3 = new Grid3D(coordsStart.x + 10 - offSet, 0, coordsStart.z - offSet, dimensions.x, dimensions.y, dimensions.z, 1, 1, 14)
 	    .drawSolidCube(0, 0, 0, 1, 1, 14, new Sprite(0, 0, 32, 32, 'densetreesxs.png'));
 
-	let outerwall4 = new Grid3D(coordsStart.x, 0, coordsStart.z + 10, dimensions.x, dimensions.y, dimensions.z, 13, 1, 1)
+	let outerwall4 = new Grid3D(coordsStart.x - offSet, 0, coordsStart.z + 10 - offSet, dimensions.x, dimensions.y, dimensions.z, 13, 1, 1)
 	    .drawSolidCube(0, 0, 0, 13, 1, 1, new Sprite(0, 0, 32, 32, 'densetreesxs.png'));
 	
 	let fs = [ 
