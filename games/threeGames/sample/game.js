0a3d77c146a2c106bbe4e15bc6d450ad08bf2660
clearwinggames <80926717+clearwinggames@users.noreply.github.com>
2026-02-24 09:53:51 -0500

    Update game.js

--- games/threeGames/sample/game.js
+++ games/threeGames/sample/game.js
@@ -4,7 +4,7 @@ let dimensions = { x: 1, y: 1, z: 1 };
 function alternate_first_scene() {
 	
 	let floor = new Grid3D(coordsStart.x, -1, 1, dimensions.x, dimensions.y, dimensions.z, 14, 12, 13)
-	.drawSolidCube(0, 0, 0, 14, 1, 12, new Sprite(0, 0, 32, 32, 'grassx.png'));
+	.drawSolidCube(0, 0, 0, 14, 1, 12, new Sprite(0, 0, 32, 32, 'grassxs.png'));
 	
 	let outerwall1 = new Grid3D(coordsStart.x, 0, coordsStart.z, dimensions.x, dimensions.y, dimensions.z, 1, 1, 14)
 	    .drawSolidCube(0, 0, 0, 1, 1, 14, new Sprite(0, 0, 32, 32, 'brickx.png'));
